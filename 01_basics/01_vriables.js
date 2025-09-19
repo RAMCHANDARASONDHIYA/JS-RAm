35fcc61e570a538e5b8e74b4599919ab2dd69bd3
RAMCHANDARASONDHIYA <sondhiyaramchandra250@gmail.com>
2025-09-19 16:35:00 +0000

    vraibles

---
+++ 01_basics/01_vriables.js
@@ -0,0 +1,20 @@
+const accountId = 21974
+let accountEmail ="ram@google.com"
+var accountPassword = "29864"
+accountCity = "Bioara"
+let accountState
+
+// accountId = 24151 //not allowed
+
+accountEmail = "pradh@g.com"
+accountCity = "suthaliya"
+accountPassword = "2-9r834"
+
+console.log(accountEmail); -
+
+
+/*
+Prepare not use var
+ because of issue in block scope and functional scope 
+*/
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
